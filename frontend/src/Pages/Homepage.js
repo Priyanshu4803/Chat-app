f0e0c528b1290d78446d8d3c77645cbd85d8b501
Priyanshu Jain <155756601+Priyanshu4803@users.noreply.github.com>
2024-07-30 22:50:13 +0530

    Update Homepage.js

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -37,7 +37,7 @@ function Homepage() {
         backgroundColor="#d6eaf8"
       >
         <Text fontSize="4xl" fontFamily="Work sans">
-          BuzzTalk
+          WhisperHub
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
