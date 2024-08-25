bf90d4419123dd1d1615e3f7081b1b7ba63ceaba
Priyanshu Jain <155756601+Priyanshu4803@users.noreply.github.com>
2024-08-26 01:15:56 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,27 @@
+// const express = require("express");
+// const {
+//   registerUser,
+//   authUser,
+//   allUsers,
+// } = require("../controllers/userControllers");
+// const { protect } = require("../middleware/authMiddleware");
+
+// const router = express.Router();
+
+// //for searching all users using ?search=
+// router.route("/").get(protect, allUsers);
+// //register
+// router.route("/").post(registerUser);
+// router.post("/login", authUser);
+
+// module.exports = router;
+
 const express = require("express");
 const {
   registerUser,
   authUser,
   allUsers,
+  googleAuth,
 } = require("../controllers/userControllers");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -12,6 +31,8 @@ const router = express.Router();
 router.route("/").get(protect, allUsers);
 //register
 router.route("/").post(registerUser);
+router.route("/googleAuth").post(googleAuth);
 router.post("/login", authUser);
 
 module.exports = router;
+ 
